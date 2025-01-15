6a3f7192e2fc1efa2bf612e404c3cb73e902a3bb
allyrob3rts <ag.roberts493@k12cpschools.com>
2025-01-15 00:14:48 -0500

    1/15

---
+++ My project (4)/Assets/Scripts/IGrid.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace AStarPathfinding
+{
+
+    public interface IGridProvider
+    {
+        Vector2Int Size { get; }
+        Cell this[Vector2Int position] { get; }
+        void Reset();
+    }
+
+}
